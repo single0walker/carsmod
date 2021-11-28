d56ecb30c31d5f03738d5edaca100147ddfebdc0
singlewalker <fangyuqifang@outlook.com>
2021-11-28 21:47:49 +0800

    将mod展示核心部分完成

---
+++ js/data.js
@@ -0,0 +1,42 @@
+carsmods=[
+    {name:"我的车队 奥迪涂装",link:'https://pan.baidu.com/s/1PuGCIIMsYb6xsZqKAyaWAA',tiquma:'i5ru',img:'../img/aodi.jpg',banben:'2020'},
+    {name:"我的车队 丰田涂装",link:'https://pan.baidu.com/s/1GVIpN5haWY-qU_ErZulfVw',tiquma:'hmgs',img:'../img/fengtian.jpg',banben:'2020'},
+    {name:"威廉姆斯2020年涂装",link:'https://pan.baidu.com/s/18KfLsIOE4dWdRlivLRrG4g',tiquma:'ztn8',img:'../img/wlms20.jpg',banben:'2019'},
+    {name:"万宝路迈凯伦涂装",link:'https://pan.baidu.com/s/12k3NwNwEt_mnJxq4NXjMHA',tiquma:'5a7w',img:'../img/vgpnjCC.png',banben:'2020'},
+    {name:"红牛RB18涂装",link:'https://pan.baidu.com/s/11JIR3bW7ep4aK3DDcnkSfA',tiquma:'x6ug',img:'../img/redbullrb18.png',banben:'2020'},
+    {name:"我的车队 迈凯伦MP4-23涂装",link:'https://pan.baidu.com/s/1ecNoUeiGiLQ5VMjjUwPlUw',tiquma:'183c',img:'../img/mp423.jpg',banben:'2020'},
+    {name:"我的车队 布朗GP",link:'https://pan.baidu.com/s/1v_RQFT9ERYkRuI4oIfUfpw',tiquma:'h8xn',img:'../img/BrawnGP.jpg',banben:'2020'},
+    {name:"我的车队 宝马涂装",link:'https://pan.baidu.com/s/1xSOmnRBH-CujDyNL7f3JJw',tiquma:'u8f4',img:'../img/BMW.png',banben:'2020'},
+    {name:"奔驰魔爪涂装",link:'https://pan.baidu.com/s/1ffr84GQMqQxcjqyw5x4hNA',tiquma:'vkut',img:'../img/bcmz.jpg',banben:'2019'},
+    {name:"法拉利1000场大奖赛涂装",link:'https://pan.baidu.com/s/16szK97ftmt75bYl7PcONbA',tiquma:'yf29',img:'../img/F1_2020_photo_20200909_224656.jpg',banben:'2020'},
+    {name:"白色法拉利涂装",link:'https://pan.baidu.com/s/1OnnjTGcn8ZkYQC6H3Ms_sg',tiquma:'mmsx',img:'../img/Screenshot_2.jpg',banben:'可能是2020'},
+    {name:"哈斯2020涂装",link:'https://pan.baidu.com/s/1oisZj720FmbQlvXWu1kmtA',tiquma:'zc78',img:'../img/hass.jpg',banben:'2019'},
+    {name:"银色奔驰",link:'https://pan.baidu.com/s/15XJZChKFg7pV7sLee7tduQ',tiquma:'8jei',img:'../img/ysbc.jpg',banben:'2020'},
+    {name:"阿尔法罗密欧",link:'https://pan.baidu.com/s/1wSBiIPbD9CH78k48Hs8Nkg',tiquma:'8p29',img:'../img/arflmo.png',banben:'2019'},
+    {name:"宝马BMW",link:'https://pan.baidu.com/s/1VVAL1G6h5HKU43B3LT8fJQ',tiquma:'dcss',img:'../img/bmw02.jpg',banben:'不详'},
+    {name:"小红牛2020涂装",link:'https://pan.baidu.com/s/1REMjBPCShhf1NZ9auGH5SQ',tiquma:'3jmt',img:'../img/THT3yp5.png',banben:'2019'},
+];
+toukui=[
+    {name:"霍肯伯格头盔",link:'https://pan.baidu.com/s/1Xq6zUOkb7ojcXF14fHU2-g',tiquma:'xfvf',img:'../img/updateHulk.jpg',banben:'不详'},
+    {name:"F1 2020 头盔合集",link:'https://pan.baidu.com/s/1OUVCz8i0hFruB7akFg5GRg',tiquma:'suvd',img:'',banben:"可能是2020"},
+];
+fuzhuang=[
+    {name:"",link:'',tiquma:'',img:''},
+];
+other=[
+    {name:"SimHUD HUD前置软件",link:'https://pan.baidu.com/share/init?surl=1RRe7Pgsbv8qCUntJxIuhA',tiquma:'2222',img:'../img/qita.png',banben:'all'},
+    {name:"WCP TV HUD",link:'https://pan.baidu.com/s/1H5kv-CobPIgB5w3itU5Dtg',tiquma:'qysb',img:'../img/screen1.jpg',banben:'all'},
+    {name:"WCP Halo HUD",link:'https://pan.baidu.com/s/1M-btiH9OFyr_Xnj3XngWEw',tiquma:'3jc2',img:'../img/screen1.jpg',banben:'all'},
+    {name:"超级碰撞",link:'https://pan.baidu.com/s/1gLb6uoUfCNsROq8xaKuuqA',tiquma:'mu8j',img:'',banben:'2020'},
+    {name:"HUD轮胎",link:'https://pan.baidu.com/s/1LCCWeOeHCVIgwh3RN5al-w',tiquma:'fih9',img:'../img/hudluntai.png',banben:'all'},
+    {name:"本田引擎特殊声音",link:'https://pan.baidu.com/s/1mbz1LEinwszSOoERPRfOqg',tiquma:'ubuy',img:'',banben:'2019'},
+    {name:"奔驰引擎声音",link:'https://pan.baidu.com/s/1-NgkVYyMR9X1dOSU2ilv0A',tiquma:'44a3',img:'',banben:'2020'},
+    {name:"法拉利定制仪表",link:'https://pan.baidu.com/s/16kb9RIfL_f78dswu2MJmOg',tiquma:'xzjg',img:'../img/flldzyb.jpg',banben:'2020'},
+    {name:"红牛定制仪表",link:'https://pan.baidu.com/s/1qwITQq4UzsrHnQmBIQen9Q',tiquma:'1nx8',img:'../img/redbullyb.jpg',banben:'2020'},
+    {name:"2020 HaloHUD",link:'https://pan.baidu.com/s/1T1Zymvcwy6Z4uxxbrx56jA',tiquma:'tu8z',img:'../img/2020halohud.png',banben:'all'},
+    {name:"更多DRS使用区",link:'https://pan.baidu.com/s/1TIkFkMCRxM9JBHhqiWr3Ug',tiquma:'gctg',img:'../img/F1circuits.PNG',banben:'2020'},
+    {name:"F1 2021 赛季MOD",link:'https://pan.baidu.com/s/13PVlYPeXyg49z1K6TY1Ouw',tiquma:'9uih',img:'https://img.3dmgame.com/uploads/images/thumbkwdfirst/20210415/1618500940_744490.jpg',banben:'可能是2020'},
+    {name:"更多DRS使用区",link:'https://pan.baidu.com/s/1wRl-jdqSBNcCTV0d-FzkPA',tiquma:'x954',img:'',banben:'不详'},
+    {name:"真实视角MOD",link:'https://pan.baidu.com/s/1CUCFcdvONguLRltqWClwGg',tiquma:'au9t',img:'../img/zssj.jpg',banben:'不详'},
+    {name:"我的车队车标",link:'https://pan.baidu.com/s/1-c3IzTwiKL5jPFbyo7xKPg',tiquma:'dvq8',img:'',banben:'2020'},
+]
